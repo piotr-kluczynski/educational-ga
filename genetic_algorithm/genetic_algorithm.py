120f65bc4bfee9e3fd934f57f6d517c9ee91d48d
Piotr Kluczynski <peklu@wp.pl>
2026-05-24 14:46:17 +0200

    Created run_genetic_algorithm function, which will launch the GA given the proper parameters. Modified all functions within genetic_algorithm module to accept partial_functions as parameters for crossover, mutation and selection.

--- genetic_algorithm/genetic_algorithm.py
+++ genetic_algorithm/genetic_algorithm.py
@@ -2,14 +2,9 @@ import random
 
 from mutation import single_point
 from individual import Individual
-from sharing import crowding_function
-from fitness_function import fitness_function
-from selection import stochastic_sampling_with_replacement
 
 
-def initialize_population(chromosome_length, alphabet, domains, encoding, is_haploidal, pop_size, objective_function,
-                          crowding=False, custom_transformation=None, constraints=None, scaling="none", scaling_factors=None,
-                          sigma_share=0, alpha=0): # Sharing parameters
+def initialize_population(chromosome_length, alphabet, domains, encoding, is_haploidal, pop_size, fitness_function, crowding_function):
     population = []
 
     for _ in range(pop_size):
@@ -46,25 +41,21 @@ def initialize_population(chromosome_length, alphabet, domains, encoding, is_hap
             new_individual.domination_chromosome = domination_chromosome
 
         # Calculating initial fitness
-        new_individual.raw_fitness = fitness_function(new_individual.get_phenotype(), objective_function, custom_transformation, constraints, scaling, scaling_factors)
+        new_individual.raw_fitness = fitness_function(new_individual.get_phenotype())
         new_individual.practical_fitness = new_individual.raw_fitness
 
         population.append(new_individual)
 
     # Applying crowding function
-    if crowding:
+    if crowding_function is not None:
         for individual in population:
-            individual.practical_fitness = crowding_function(individual, population, sigma_share, alpha, encoding)
+            individual.practical_fitness = crowding_function(individual, population)
 
     return population
 
-def next_generation(prev_gen, pop_size, p_mutation, p_crossover, alphabet,
-                    objective_function, crossover, mutation, encoding,
-                    custom_transformation=None, constraints=None, scaling="none", scaling_factors=None,
-                    selection=stochastic_sampling_with_replacement,
-                    is_diploid=False,
-                    crowding=False, elite_size=0, replacement=1,
-                    sigma_share=0, alpha=0): # Sharing parameters
+def next_generation(prev_gen, pop_size, p_mutation,
+                    fitness_function, crossover, mutation, selection, crowding_function,
+                    is_diploid=False, elite_size=0, replacement=1):
     offsprings = []
     couples = selection(prev_gen, pop_size)
 
@@ -72,20 +63,20 @@ def next_generation(prev_gen, pop_size, p_mutation, p_crossover, alphabet,
         parent1, parent2 = couples[i]
 
         # Creating chromosome1
-        child1_chromosome1, child2_chromosome1 = crossover(parent1.chromosome1, parent2.chromosome1, p_crossover)
+        child1_chromosome1, child2_chromosome1 = crossover(parent1.chromosome1, parent2.chromosome1)
 
         child1_chromosome1 = mutation(child1_chromosome1, p_mutation)
         child2_chromosome1 = mutation(child2_chromosome1, p_mutation)
 
         # Creating chromosome2 and domination_chromosome in case of diploidal
         if is_diploid:
-            child1_chromosome2, child2_chromosome2 = crossover(parent1.chromosome2, parent2.chromosome2, p_crossover)
-            child1_domination_chromosome, child2_domination_chromosome = crossover(parent1.domination_chromosome, parent2.domination_chromosome, p_crossover)
+            child1_chromosome2, child2_chromosome2 = crossover(parent1.chromosome2, parent2.chromosome2)
+            child1_domination_chromosome, child2_domination_chromosome = crossover(parent1.domination_chromosome, parent2.domination_chromosome)
 
             child1_chromosome2 = mutation(child1_chromosome2, p_mutation)
-            child1_domination_chromosome = single_point(child1_domination_chromosome, alphabet, p_mutation)
+            child1_domination_chromosome = single_point(child1_domination_chromosome, [0, 1], p_mutation)
             child2_chromosome2 = mutation(child2_chromosome2, p_mutation)
-            child2_domination_chromosome = single_point(child2_domination_chromosome, alphabet, p_mutation)
+            child2_domination_chromosome = single_point(child2_domination_chromosome, [0, 1], p_mutation)
 
 
         # Creating the children individuals
@@ -97,18 +88,18 @@ def next_generation(prev_gen, pop_size, p_mutation, p_crossover, alphabet,
             child2 = Individual(child2_chromosome1)
 
         # Calculating initial fitness
-        child1.raw_fitness = fitness_function(child1.get_phenotype(), objective_function, custom_transformation, constraints, scaling, scaling_factors)
+        child1.raw_fitness = fitness_function(child1.get_phenotype())
         child1.practical_fitness = child1.raw_fitness
-        child2.raw_fitness = fitness_function(child2.get_phenotype(), objective_function, custom_transformation, constraints, scaling, scaling_factors)
+        child2.raw_fitness = fitness_function(child2.get_phenotype())
         child2.practical_fitness = child2.raw_fitness
 
         offsprings.append(child1)
         offsprings.append(child2)
 
     # Applying crowding function
-    if crowding:
+    if crowding_function is not None:
         for individual in offsprings:
-            individual.practical_fitness = crowding_function(individual, offsprings, sigma_share, alpha, encoding)
+            individual.practical_fitness = crowding_function(individual, offsprings)
 
     # Applying elitism
     elites = []
@@ -131,3 +122,14 @@ def next_generation(prev_gen, pop_size, p_mutation, p_crossover, alphabet,
         next_gen = next_gen[:pop_size]
 
     return next_gen
+
+def run_genetic_algorithm(pop_size, chromosome_length, alphabet, p_mutation, crossover, mutation, selection, elite_size, replacement, encoding, is_diploid, domains, fitness_function, crowding_function, end_condition):
+    gen_count = 0
+    prev_population = initialize_population(chromosome_length, alphabet, domains, encoding, is_diploid, pop_size, fitness_function, crowding_function)
+
+    while end_condition(gen_count, prev_population) is False:
+        next_population = next_generation(prev_population, pop_size, p_mutation, fitness_function, crossover, mutation, selection, crowding_function, is_diploid=is_diploid, elite_size=elite_size, replacement=replacement)
+        prev_population = next_population
+        gen_count += 1
+
+    return prev_population
